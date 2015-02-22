52e8648999e8ba8c6bec6d1ac3828fb0fa417e8e
Darryl Kuhn <darrylkuhn@aristamd.com>
2015-02-22 20:49:00 +0000

    ensure vendor files are loaded first

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
 
+require_once(__DIR__.'/../vendor/autoload.php');
 require_once(dirname(__FILE__).'/src/MockJsonDialectModel.php');
-
-// register vendors if possible
-if (file_exists(__DIR__.'/../vendor/autoload.php')) {
-    require_once(__DIR__.'/../vendor/autoload.php');
-}
