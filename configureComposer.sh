a3a4bc74fab1ba0a8e91ad316ecd8284d294eab2
Darryl Kuhn <darryl@aristamd.com>
2015-06-04 19:33:43 +0000

    attempting perform authenticate github requests as described here: https://vierbergenlars.wordpress.com/2013/06/06/using-a-github-oauth-token-with-composer-travis-ci/

---
+++ configureComposer.sh
@@ -0,0 +1,7 @@
+if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];
+then
+mkdir ~/.composer/
+echo '{ "config": {"github-oauth":{"github.com": ' > ~/.composer/config.json
+echo "\"$GH_OAUTH\"" >> ~/.composer/config.json
+echo '}}}' >> ~/.composer/config.json
+fi
